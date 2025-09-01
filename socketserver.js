23baaadcf0ac82caec3df8e27e50d0cb229b1b79
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-01 12:37:48 +0530

    count

---
+++ socketserver.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app =  express();
+var http = require("http");
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+var count = 1;
+
+const io = new Server(server);
+
+io.on("connection",(socket) => {
+    console.log("Socket Connected");
+    io.emit("countInc",{ count })
+    // setInterval(() => {
+    // count++;
+    // io.emit('presentCount',{ count });
+    // },100);
+});
+
+app.get("/incCount",(req,res) => {
+    count++;
+    io.emit("countInc",{ count });
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/",(req,res) => {
+    res.send("Server Socket")
+})
+
+server.listen(4000,()=>{
+    console.log("Server running on 4000")
+})
